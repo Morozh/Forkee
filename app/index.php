93b8decd7b654d72883f91e4bb2eba8c798a411a
Ruslan <rusl.trynin@yandex.com>
2020-10-03 20:17:39 +0500

    add db + s.layout

--- app/index.php
+++ app/index.php
@@ -1,3 +1,6 @@
+<?php 
+  require "includes/db.php";
+?>
 <!doctype html>
 <html lang="ru">
   <head>
@@ -18,7 +21,24 @@
   <body>
 
     <!-- Header -->
+    <header class="header">
+      <div class="container">
+        <h1>Hello world!</h1>
+      </div>
+    </header>
     <!-- Find Section -->
+    <section class="section">
+      <div class="container">
+        <h2>Hello world!</h2>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="container">
+        &#169;&nbsp;Trio
+      </div>
+    </footer>
 
     <!-- JavaScript -->
     <script src="libs/jquery-3.5.1.min.js"></script>
