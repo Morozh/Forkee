a97a336d1eb070d844d0ae1445ff7d35acdcfdf9
Ruslan <rusl.trynin@yandex.com>
2020-11-11 18:16:01 +0500

    Убрал лишние файлы, добавил не настроенный .htaccess

--- app/vacancy.php
+++ app/vacancy.php
@@ -3,7 +3,7 @@
 
   $host = 'localhost';
   $user = 'root'; 
-  $password = ''; //index.php 6 string, db.php 4 string
+  $password = '';
   $db_name = 'forkee'; 
   $link = mysqli_connect($host, $user, $password, $db_name);
 ?>
@@ -70,19 +70,22 @@
         <div class="search-box-bg">
           <div class="total-vac">
             <span class="total-vac__item">
-            Found
+            Found 
               <?php
-              $sql = mysqli_query($link, 'SELECT COUNT(*) FROM `vacancies`');
+              $search_get = $_GET['search'];
+              $sql_query = " SELECT COUNT(*) FROM vacancies 
+              WHERE title_vacancy LIKE '%$search_get%' ";
+              $sql = mysqli_query( $link,$sql_query);
               $result = mysqli_fetch_array($sql);
                 echo '<span style="color: #00C1DA;">'.$result[0].'</span>';
               ?>
-            jobs
+            jobs 
             </span>
           </div>
-          <form method = "get" action ="main_search.php"> 
+          <form method = "get" action ="vacancy.php"> 
             <div class="search-vac">
               <div class="search-vac__input">
-                <input class="v-input" type="search" placeholder="Profession, position or vacancy" name ="search"/>
+                <input class="v-input" type="search" placeholder="Profession, position or vacancy" name="search" value="<?php echo $_GET['search']?> "/>
               </div>
               <div class="search-vac__btn">
                 <input class="v-btn" type="submit" value="SEARCH" />
@@ -138,33 +141,37 @@
             <span class="filter-row__title">Exceptions</span>
           </div>
         </div>
+       
         <div id="content" class="card-box-container">
           <?php
-            $sql = mysqli_query($link, 'SELECT va.title_vacancy, va.salary, en.title, ci.title_city, va.remote_work, va.phone_number, va.description, va.date FROM vacancies va, enterprises en, cities ci WHERE va.enterprise = en.id_enterprise and va.city = ci.id_city and active is true');
+          $sql_query = " SELECT va.title_vacancy, va.salary, en.title, ci.title_city, va.remote_work, va.phone_number, 
+          va.description, va.date FROM vacancies va, enterprises en, cities ci 
+          WHERE va.enterprise = en.id_enterprise and va.city = ci.id_city and active is true and va.title_vacancy  LIKE '%$search_get%' ";
+          $sql = mysqli_query($link,$sql_query);
             while ($result = mysqli_fetch_array($sql)) {
-              echo 
-              '<div class="card-box-item">'.
+            echo 
+            '<div class="card-box-item">'.
                 '<div class="card-box-row">'.
-                  "<span class='card-box-row__title'>{$result['title_vacancy']}</span>".
-                  "<span class='card-box-row__price'>{$result['salary']}$</span>".
+                    "<span class='card-box-row__title'>{$result['title_vacancy']}</span>".
+                    "<span class='card-box-row__price'>{$result['salary']}$</span>".
                 '</div>'.
                 '<div class="card-box-row">'.
-                  "<span class='card-box-row__phone'>Organization: {$result['title']}</span>".
+                    "<span class='card-box-row__phone'>Organization: {$result['title']}</span>".
                 '</div>'.
                 '<div class="card-box-row">'.
-                  "<span class='card-box-row__city'>City: {$result['title_city']}</span>".
+                    "<span class='card-box-row__city'>City: {$result['title_city']}</span>".
                 '</div>'.
                 '<div class="card-box-row">'.
-                  "<span class='card-box-row__phone'>Phone: + {$result['phone_number']} </span>".
+                    "<span class='card-box-row__phone'>Phone: + {$result['phone_number']} </span>".
                 '</div>'.
                 '<div class="card-box-row">'.
-                  "<p class='card-box-row__description'>{$result['description']}</p>".
+                    "<p class='card-box-row__description'>{$result['description']}</p>".
                 '</div>'.
                 "<div class='p-bottom'>
-                  <a class='card-box-row__respond' href=''>Respond</a>
-                  <span class='card-box-row__date'>{$result['date']}</span>
+                    <a class='card-box-row__respond' href=''>Respond</a>
+                    <span class='card-box-row__date'>{$result['date']}</span>
                 </div>".
-              '</div>';
+            '</div>';
             }
           ?>
         </div>
